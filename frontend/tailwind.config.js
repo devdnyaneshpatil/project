210b12efd9184e81f2fc98475e8b843a9ab2952e
devdnyaneshpatil <dnyaneshwarpatil830831@gmail.com>
2024-08-15 07:40:53 +0530

    account page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '300px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [],
 }
