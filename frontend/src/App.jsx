af7c7f5c303484924515bb9bf31f2f0938c04428
PatilAmey1 <patilameyshivaji@gmail.com>
2024-08-27 00:15:12 +0530

    add Toast componet and OTPVerification component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,25 @@ import "./App.css"; ///dont remove added for tailwind css
 import { Route, Routes } from "react-router-dom";
 import Account from "./Pages/Account";
 import Appointment from "./Pages/Appointment";
+import SignUpLogin from "./Componets/SignUpLogin";
+import OTPVerification from "./Componets/OTPVerification";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path="/OTP" Component={OTPVerification} />
+        <Route path="/Sign-Up" Component={SignUpLogin} />
         <Route path="/my-account" Component={Account} />
-        <Route path="/appointment" Component={Appointment}/>
+        <Route path="/appointment" Component={Appointment} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+///-----verify OTP ---page
+///-----signUp
+///-----login
+////-----
