0a542632e5168a00517430223edde6c817bd89b8
PatilAmey1 <patilameyshivaji@gmail.com>
2024-08-31 01:19:31 +0530

    added Drawer componet

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Componets/HomePageComponents/Navbar";
 
 function HomePage() {
+  const [bgColor, setBgColor] = useState("bg-white");
+  const handalClick = (color) => {
+    if (color === "white") {
+      setBgColor("bg-white");
+    } else {
+      setBgColor("bg-bgColorBrown opacity-75 border-gray-50");
+    }
+  };
+
   return (
-    <div>
-      <Navbar></Navbar>
+    <div className={`${bgColor} h-screen `}>
+      <Navbar handalClick={handalClick} bgColor={bgColor}></Navbar>
     </div>
   );
 }
